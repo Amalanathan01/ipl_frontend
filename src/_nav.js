8837f2196fd2ab7058c0bc5346f5ad9a2f5d2d2a
Amalanathan S <amalanathan.s@cognizant.com>
2019-08-31 23:36:28 +0530

    663904-Added Side bar nav items.

--- src/_nav.js
+++ src/_nav.js
@@ -1,16 +1,51 @@
 export default {
     items: [
       {
-        name: 'Dashboard',
-        url: '/dashboard',
+        name: 'Overview',
+        url: '/overview',
         icon: 'icon-speedometer',
         badge: {
           variant: 'info',
         },
       },
       {
-        name: 'Admin',
-        url: '/admin',
+        name: 'Batsman Score',
+        url: '/batsmanscore',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Batsman Ranking',
+        url: '/batsmanranking',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Team1 vs Team2',
+        url: '/teamcompare',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Batsman Score Chart',
+        url: '/batsmanscorechart',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Ranking Chart',
+        url: '/batsmanrankingchart',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Team1 vs Team2 Chart',
+        url: '/teamchartcompare',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Manage Score',
+        url: '/managescore',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Batsman Teams',
+        url: '/manageteams',
         icon: 'icon-puzzle'
       }
     ]
